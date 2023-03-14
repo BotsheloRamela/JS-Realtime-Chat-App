c901b1858e7229b7815000264200c90a061c034d
BotsheloRamela <ramelabotshelo12@gmail.com>
2023-03-14 08:45:35 +0200

    Firebase Config

---
+++ firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCpsJ268bm5KqVaESye4-fRbS8g267ZkEA",
+  authDomain: "realtime-chat-app-96a1d.firebaseapp.com",
+  projectId: "realtime-chat-app-96a1d",
+  storageBucket: "realtime-chat-app-96a1d.appspot.com",
+  messagingSenderId: "1003263990257",
+  appId: "1:1003263990257:web:7949f332f660b168e6cc50"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
